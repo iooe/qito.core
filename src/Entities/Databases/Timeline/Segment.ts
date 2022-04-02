174d314c0c7b883e0b74c4553a84d109ab30d398
fluffionette <podstavat@gmail.com>
2022-04-02 10:00:18 +0300

    Update Segment.ts

--- src/Entities/Databases/Timeline/Segment.ts
+++ src/Entities/Databases/Timeline/Segment.ts
@@ -2,8 +2,17 @@ import Block from "./Block/Block";
 
 export default class Segment {
     private blocks: Array<Block> = [];
+    private _id: string = ''
 
     constructor() {
+    }
+
+    public setId(id: string) {
+        this._id = id
+    }
+
+    public getId() {
+        return this._id
     }
 
     public getBlocks() {
