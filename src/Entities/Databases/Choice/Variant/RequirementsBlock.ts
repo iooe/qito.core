4357ee30ec58e0e592421cacefdcdd31f5fa82b9
fluffionette <podstavat@gmail.com>
2022-04-04 19:48:22 +0300

    RquirementsBlock — added uuid generator

--- src/Entities/Databases/Choice/Variant/RequirementsBlock.ts
+++ src/Entities/Databases/Choice/Variant/RequirementsBlock.ts
@@ -1,11 +1,17 @@
 import Requirement from "./Requirements/Requirement";
+import {v4 as uuidv4} from 'uuid';
 
 export default class RequirementsBlock {
 
     private requirements: Array<Requirement> = []
+    private readonly _uuid: string = uuidv4()
 
     push(requirement: Requirement) {
         return this.requirements.push(requirement)
+    }
+
+    public getId() {
+        return this._uuid
     }
 
     getRequirements() {
