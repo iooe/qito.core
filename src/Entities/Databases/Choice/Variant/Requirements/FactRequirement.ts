9e22a6084e57ff0f6aded8791e54871178d994e4
fluffionette <podstavat@gmail.com>
2022-04-04 19:48:49 +0300

    Update FactRequirement.ts
    Added constant OPERATORS

--- src/Entities/Databases/Choice/Variant/Requirements/FactRequirement.ts
+++ src/Entities/Databases/Choice/Variant/Requirements/FactRequirement.ts
@@ -1,6 +1,12 @@
 import Requirement from "./Requirement";
 import DefaultRequirement from "./DefaultRequirement";
 
+export const constants = {
+    OPERATORS: [
+        '=',
+        '!='],
+} as const;
+
 export default class FactRequirement extends DefaultRequirement implements Requirement {
     constructor(id: string, operator: string) {
         super(id, operator)
