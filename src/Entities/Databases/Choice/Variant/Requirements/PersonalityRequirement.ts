a8db7e6c8c7185f3e18ef71c2b4adee685f8d89a
fluffionette <podstavat@gmail.com>
2022-04-04 19:48:47 +0300

    Update PersonalityRequirement.ts
    Added constant OPERATORS

--- src/Entities/Databases/Choice/Variant/Requirements/PersonalityRequirement.ts
+++ src/Entities/Databases/Choice/Variant/Requirements/PersonalityRequirement.ts
@@ -1,6 +1,12 @@
 import Requirement from "./Requirement";
 import DefaultRequirement from "./DefaultRequirement";
 
+export const constants = {
+    OPERATORS: [
+        '>=',
+        '<='],
+} as const;
+
 export default class PersonalityRequirement extends DefaultRequirement implements Requirement {
     constructor(id: string, operator: string, value: number) {
         super(id, operator, value)
