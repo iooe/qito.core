0356333e973e4d78e4c64700e7d4093cb1e21e7e
fluffionette <podstavat@gmail.com>
2022-04-15 16:34:24 +0300

    Update Personality.ts

--- src/Entities/Databases/Protagonist/Personality.ts
+++ src/Entities/Databases/Protagonist/Personality.ts
@@ -3,40 +3,52 @@ import Media from "../../Basic/Objects/Media";
 export default class Personality {
 
     private readonly _uuid: string
-    private _name: string
+    private _name: string = ''
     private _value: number = 0
     private _media: Media = new Media()
 
-    constructor(uuid: string, name: string) {
+    constructor(uuid: string) {
         this._uuid = uuid
-        this._name = name
     }
 
     public getUuid(): string {
         return this._uuid;
     }
 
-    public getName(): string {
-        return this._name;
+    public name = {
+        set: (value: string) => {
+            this._name = value
+        },
+        get: () => {
+            return this._name
+        }
+    }
+    public value = {
+        set: (value: number) => {
+            this._value = value;
+        },
+        get: () => {
+            return this._value
+        }
     }
 
-    public setName(value: string) {
-        this._name = value;
+    public media = {
+        get: () => {
+            return this._media;
+        },
+        set: (value: Media) => {
+            this._media = value
+        }
     }
 
-    public setValue(value: number) {
-        this._value = value;
-    }
-
-    public getValue(): number {
-        return this._value;
-    }
-
-    public setMedia(media: Media) {
-        this._media = media
-    }
-
-    public media(): Media {
-        return this._media;
+    public export(): Object {
+        return {
+            uuid: this._uuid,
+            name: this._name,
+            media: {
+                id: this._media.path()
+            },
+            value: this._value
+        }
     }
 }
