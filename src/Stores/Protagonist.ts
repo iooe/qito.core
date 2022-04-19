0ec2bb3debad03cf389955c18ac6341d40bd181d
fluffionette <podstavat@gmail.com>
2022-04-19 08:29:38 +0300

    Create Protagonist.ts

---
+++ src/Stores/Protagonist.ts
@@ -0,0 +1,85 @@
+import Personality from "../Entities/Databases/Protagonist/Personality";
+import Media from "../Entities/Basic/Objects/Media";
+
+export const NAME = 'protagonist'
+
+export const touchPersonality = (state: any) => {
+    const length = state.personalities.length;
+    state.personalities.push(state.personalities[length - 1])
+    state.personalities.splice(state.personalities[length], 1)
+}
+
+export const scheme: any = {
+    namespaced: true,
+    state: () => ({
+        personalities: Array<Personality>(),
+    }),
+    mutations: {
+        pushPersonality(state: any, item: Personality) {
+            state.personalities.push(item)
+        },
+        increasePersonality(state: any, {uuid, value}: any) {
+            const instance = state.personalities.find((item: Personality) => item.getUuid() === uuid);
+
+            if (instance === undefined) {
+                return
+            }
+
+            instance.value.set(instance.value.get() + value);
+        },
+        decreasePersonality(state: any, {uuid, value}: any) {
+            const instance = state.personalities.find((item: Personality) => item.getUuid() === uuid);
+
+            if (instance === undefined) {
+                return
+            }
+
+            instance.value.set(instance.value.get() - value);
+        },
+    },
+    actions: {
+        addPersonality(context: any, personality: Personality) {
+            if (context.getters.getPersonality(personality.getUuid()) !== undefined) {
+                return false
+            }
+
+            context.state.personalities.push(personality)
+
+            return true;
+        },
+        import(context: any, data: Array<any>) {
+            data.forEach(instance => {
+                if (instance.key === 'personalities') {
+                    instance.data.forEach((value: any) => {
+                        const instance = new Personality(value.uuid)
+                        instance.name.set(value.name)
+                        instance.media.set(new Media(value.media.id))
+                        instance.value.set(value.value)
+
+                        context.commit('pushPersonality', instance)
+                    })
+                }
+            })
+        },
+        export(context: any) {
+            return {
+                name: NAME,
+                data: [
+                    {
+                        'personalities': context.state.personalities.map((value: Personality) => value.export())
+                    }
+                ]
+            }
+        }
+    },
+    getters: {
+        getPersonalities: (state: any) => {
+            return state.personalities
+        },
+        getPersonality: (state: any) => (id: string): Personality => {
+            return state.personalities.find((item: Personality) => item.getUuid() === id)
+        }
+    }
+}
+
+export default scheme
