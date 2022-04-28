9bf5c37c17e2f70e4ce07fdaf19209d2ae422502
fluffionette <podstavat@gmail.com>
2022-04-28 18:03:01 +0300

    Update Nav.ts
    Added export method

--- src/Entities/Databases/Timeline/Meta/Nav.ts
+++ src/Entities/Databases/Timeline/Meta/Nav.ts
@@ -1,25 +1,32 @@
 export default class Nav {
-    private prev: string | null;
-    private next: string | null;
+    private readonly _prev: string | null;
+    private readonly _next: string | null;
 
     constructor(prev: string | null = null, next: string | null = null) {
-        this.prev = prev
-        this.next = next
+        this._prev = prev
+        this._next = next
     }
 
     public hasPrev(): boolean {
-        return this.prev !== null
+        return this._prev !== null
     }
 
     public hasNext(): boolean {
-        return this.next !== null
+        return this._next !== null
     }
 
     public getPrev(): string | null {
-        return this.prev
+        return this._prev
     }
 
     public getNext(): string | null {
-        return this.next
+        return this._next
+    }
+
+    public export(): object {
+        return {
+            prev: this._prev,
+            next: this._next
+        }
     }
 }
