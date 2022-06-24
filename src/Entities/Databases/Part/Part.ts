bcdebd24ca57cb80cb5acee1fb0df79f74142f8c
fluffionette <podstavat@gmail.com>
2022-06-24 15:53:20 +0300

    /Entities/Databases/Module/Part/Part.ts was extracted into independent store

---
+++ src/Entities/Databases/Part/Part.ts
@@ -0,0 +1,81 @@
+import {v4 as uuidv4} from 'uuid';
+import Page from "../../Basic/Objects/Page";
+
+export default class Part {
+    private _pages: Array<Page> = []
+
+    private readonly _uuid: string;
+    private _title: string = ''
+
+    constructor(uuid: string) {
+        this._uuid = uuid
+        this._title = uuid
+    }
+
+    public static create() {
+        const instance = new Part(uuidv4())
+
+        instance.pages.set([
+            Page.create(),
+            Page.create()
+        ])
+
+        return instance
+    }
+
+    public getUuid() {
+        return this._uuid
+    }
+
+    public title = {
+        get: (): string => {
+            return this._title
+        },
+        set: (value: string) => {
+            this._title = value
+        }
+    }
+
+    public pages = {
+        set: (pages: Array<Page>) => {
+            this._pages = pages
+        },
+        replace: (uuid: string, value: Page) => {
+            const index = this._pages.findIndex((value: Page) => value.getUuid() === uuid)
+
+            if (index === -1) {
+                new Error('Unfinded')
+            }
+
+            this._pages[index] = value
+        },
+        get: (): Array<Page> => {
+            return this._pages
+        },
+        push: (page: Page) => {
+            this._pages.push(page)
+        },
+        count: () => {
+            return this._pages.length
+        },
+        first: (uuid: string = '') => {
+
+            if (uuid.length === 0) {
+                return this._pages[0]
+            }
+
+            return this._pages.find((value: Page) => value.getUuid() === uuid)
+        },
+        index: (uuid: string) => {
+            return this._pages.findIndex((value: Page) => value.getUuid() === uuid)
+        },
+    }
+
+    public export() {
+        return {
+            uuid: this._uuid,
+            title: this._title,
+            pages: this._pages.map((value: Page) => value.export())
+        }
+    }
+}
---
+++ src/Stores/Parts.ts
@@ -0,0 +1,81 @@
+import Part from "../Entities/Databases/Part/Part";
+import Page from "../Entities/Basic/Objects/Page";
+
+export const NAME = 'parts'
+
+export const touch = (state: any) => {
+    const length = state.data.length;
+    state.data.push(state.data[length - 1])
+    state.data.splice(length, 1)
+}
+
+export const scheme = {
+    namespaced: true,
+    state: () => ({
+        data: Array<any>(),
+    }),
+    actions: {
+        add(context: any, value: Part) {
+            if (context.getters.first(value.getUuid()) !== undefined) {
+                return false
+            }
+
+            context.state.data.push(value)
+
+            return true;
+        },
+        remove(context: any, uuid: string) {
+            const index = context.state.data.findIndex((value: Part) => value.getUuid() === uuid);
+
+            if (index === -1) {
+                return false
+            }
+
+            context.state.data.splice(index, 1)
+
+            return true;
+        },
+        import(context: any, data: Array<any>) {
+            const values: Array<Part> = []
+
+            data.forEach((partRaw: any) => {
+                const partInstance = new Part(partRaw.uuid)
+                partInstance.title.set(partRaw.title)
+
+                partRaw.pages.forEach((page: any) => {
+                    const pageInstance = new Page(page.uuid)
+                    pageInstance.setBlocks(page.blocks)
+                    partInstance.pages.push(pageInstance)
+                })
+
+                values.push(partInstance)
+            })
+
+            context.state.data = values
+        },
+        export(context: any) {
+            return {
+                name: NAME,
+                data: context.state.data.map((item: Part) => item.export())
+            }
+        }
+    },
+    getters: {
+        get: (state: any) => {
+            return state.data
+        },
+        first: (state: any) => (uuid: string = ''): Part | undefined => {
+            if (state.data.length === 0) {
+                return undefined
+            }
+
+            if (uuid.length === 0) {
+                return state.data[0]
+            }
+
+            return state.data.find((item: Part) => item.getUuid() === uuid);
+        }
+    }
+}
+
+export default scheme
