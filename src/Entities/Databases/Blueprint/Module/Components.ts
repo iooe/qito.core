cb4cfe8c6403b246c858ca3a87649c54745914e8
fluffionette <podstavat@gmail.com>
2022-07-11 15:30:50 +0300

    Create Components.ts

---
+++ src/Entities/Databases/Blueprint/Module/Components.ts
@@ -0,0 +1,17 @@
+export default {
+    part: {
+        name: 'part'
+    },
+    choice: {
+        name: 'choice',
+        variant: {
+            name: 'choice_variant'
+        }
+    },
+    switcher: {
+        name: 'switcher',
+        case: {
+            name: 'switcher-case'
+        }
+    }
+}
