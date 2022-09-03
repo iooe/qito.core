18d4913baca34b91443d6b32b0bb8565835affda
fluffionette <podstavat@gmail.com>
2022-09-03 10:59:33 +0300

    Added variable actor & store

---
+++ src/Entities/Databases/Actors/Variable/Variable.ts
@@ -0,0 +1,104 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const constants = {
+    TYPE_NUMBER: 'number',
+    TYPE_BOOLEAN: 'boolean',
+    TYPE_STRING: 'string'
+} as const;
+
+export default class Variable {
+    private readonly _uuid: string
+    private _name: string = ''
+    private _value: string | boolean | number = false
+    private _type: string = constants.TYPE_BOOLEAN
+
+    constructor(uuid: string) {
+        this._uuid = uuid
+        this._name = uuid
+    }
+
+    public static create() {
+        return new Variable(uuidv4())
+    }
+
+    public uuid = {
+        get: (): string => this._uuid
+    }
+
+    public name = {
+        set: (value: string): void => {
+            this._name = value
+        },
+        get: (): string => {
+            return this._name
+        }
+    }
+
+    public value = {
+        set: (value: boolean | string | number): void => {
+            if (this.type.isBoolean() && typeof value !== 'boolean') {
+                if (value === 'false' || value === 'true') {
+                    value = value !== 'false'
+                } else {
+                    throw Error('Incorrect value type')
+                }
+            }
+
+            if (this.type.isString()) {
+                if (typeof value !== 'string') {
+                    throw Error('Incorrect value type')
+                }
+
+                if (value.length === 0) {
+                    throw Error('Empty value')
+                }
+            }
+
+            if (this.type.isNumber() && typeof value !== 'number') {
+                throw Error('Incorrect value type')
+            }
+
+            this._value = value
+        },
+        get: (): string | boolean | number => {
+            return this._value
+        }
+    }
+
+    public type = {
+        set: (value: string): void => {
+            if (value !== constants.TYPE_NUMBER && value !== constants.TYPE_BOOLEAN && value !== constants.TYPE_STRING) {
+                throw Error('Incorrect type')
+            }
+
+            this._type = value
+
+            if (this.type.isBoolean()) {
+                this._value = false
+            }
+
+            if (this.type.isNumber()) {
+                this._value = 0
+            }
+
+            if (this.type.isString()) {
+                this._value = 'undefined'
+            }
+        },
+        get: (): string => {
+            return this._type;
+        },
+        isString: (): boolean => this._type === constants.TYPE_STRING,
+        isNumber: (): boolean => this._type === constants.TYPE_NUMBER,
+        isBoolean: (): boolean => this._type === constants.TYPE_BOOLEAN
+    }
+
+    public export(): Object {
+        return {
+            uuid: this._uuid,
+            name: this._name,
+            type: this._type,
+            value: this._value
+        }
+    }
+}
---
+++ src/Stores/Actors/Variables.ts
@@ -0,0 +1,76 @@
+import Variable from "../../Entities/Databases/Actors/Variable/Variable";
+
+export const NAME = 'actors.variables'
+
+export const touch = (state: any) => {
+    const length = state.data.length;
+    state.data.push(state.data[length - 1])
+    state.data.splice(length, 1)
+}
+
+const scheme: any = {
+    namespaced: true,
+    state: () => ({
+        data: Array<Variable>(),
+    }),
+    mutations: {
+        set(state: any, data: Array<Variable>) {
+            state.data = data;
+        }
+    },
+    actions: {
+        add(context: any, value: Variable) {
+            if (context.getters.first(value.uuid.get()) !== undefined) {
+                return false
+            }
+
+            context.state.data.push(value)
+
+            return true;
+        },
+        remove(context: any, uuid: string) {
+            const index = context.state.data.findIndex((value: Variable) => value.uuid.get() === uuid);
+
+            if (index === -1) {
+                return false
+            }
+
+            context.state.data.splice(index, 1)
+
+            return true;
+        },
+        import(context: any, data: Array<any>) {
+            let values: Array<Variable> = [];
+
+            data.forEach(value => {
+                const instance = new Variable(value.uuid)
+                instance.name.set(value.name)
+                instance.type.set(value.type)
+                instance.value.set(value.value)
+
+                values.push(instance)
+            })
+
+            context.state.data = values
+        },
+        export(context: any) {
+            return {
+                name: NAME,
+                data: context.state.data.map((value: Variable) => value.export())
+            }
+        }
+    },
+    getters: {
+        get: (state: any) => {
+            return state.data
+        },
+        first: (state: any) => (uuid: string): Variable => {
+            return state.data.find((value: Variable) => value.uuid.get() === uuid);
+        },
+        has: (state: any) => (uuid: string) => {
+            return state.data.find((value: Variable) => value.uuid.get() === uuid) !== undefined;
+        }
+    },
+}
+
+export default scheme
