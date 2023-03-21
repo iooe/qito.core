69f81030239dfd51f2e0690d8513300be174fc74
fluffionette <podstavat@gmail.com>
2023-03-21 13:13:20 +0300

    cleanup

--- src/Models/Blueprint/Module/Connection.ts
+++ src/Models/Blueprint/Module/Connection.ts
@@ -1,25 +1,9 @@
 export default class Connection {
     private readonly _uuid: string;
-    private _data: any = {};
 
     constructor(uuid: string) {
         this._uuid = uuid;
     }
-
-    public data = {
-        set: (value: Object) => {
-            this._data = value;
-        },
-        get: () => {
-            return this._data;
-        },
-        first: (key: string): any => {
-            return this._data[key];
-        },
-        has: (key: string) => {
-            return this._data.hasOwnProperty(key);
-        },
-    };
 
     public uuid(): string {
         return this._uuid;
